532f3dab2bbdb1fde8f87064952508096520bc7a
cmcuriqueo <cmcuriqueo@udc.edu.ar>
2018-01-31 04:22:07 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  	resources :users
+
+	get 'welcome/index'
+
+	resources :articles do
+		resources :comments
+	end
+	get '/signup', to: 'users#new'
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+   	delete '/logout', to: 'sessions#destroy'
+
+	root 'welcome#index'
+end
