9d1690b3b59d5d9382a5fadf64eb2a4dfd24b211
cmcuriqueo <cmcuriqueo@udc.edu.ar>
2018-02-01 21:51:26 -0300

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,30 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
-  	resources :users
-
 	get 'welcome/index'
 
 	resources :articles do
 		resources :comments
 	end
-    get  '/signup',  to: 'users#new'
-    post '/signup',  to: 'users#create'
 
-    get    '/login',   to: 'sessions#new'
-    post   '/login',   to: 'sessions#create'
-    delete '/logout',  to: 'sessions#destroy'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
 	root 'welcome#index'
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :relationships, only: [:create, :destroy]
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   #  assert_response :success
   #end
 
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 end
